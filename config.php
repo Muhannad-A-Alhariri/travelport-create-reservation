f151151654dec69cb76584eee50be19f761687f7
eroluysal <eutkuuysal@gmail.com>
2015-12-14 14:33:03 +0200

    init

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+return ([
+    'branch' => 'P105219',
+    'provider' => '1G',
+    'username' => 'Universal API/uAPI3839144488',
+    'password' => 'd25tqZDSBjg73zjzYdhXDPk6G',
+    'namespace' => 'http://www.travelport.com/schema/common_v33_0'
+]);
